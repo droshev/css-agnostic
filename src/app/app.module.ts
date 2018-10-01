a6f0addf98f9374e10b7834c4d4c00e85c649e70
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-10-01 19:12:36 +0200

    Simple button for material and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlainComponent } from './plain/plain.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlainComponent
   ],
   imports: [
     BrowserModule
