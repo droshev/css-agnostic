126952e1dd1e9e6fe418bc2c4372f59a85af0300
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-10-02 22:14:44 +0200

    Improve overal usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  uiSystems = ['materialize', 'bootstrap', 'bulma', 'foundation', 'fundamental-ui'];
-  activePanel = 'buttons';
+  systems = [
+    {
+      label: 'bootstrap',
+      icon: 'https://p14.zdassets.com/hc/settings_assets/1810307/115000215608/hkXSVzrjij0E80QkkJw67Q-logo.png'
+    },
+    {
+      label: 'materialize',
+      icon: 'https://seeklogo.com/images/M/materialize-logo-0FCAD8A6F8-seeklogo.com.png'
+    },
+
+    {
+      label: 'foundation',
+      icon: 'https://seeklogo.com/images/F/foundation-logo-AA9B725281-seeklogo.com.jpg'
+    },
+    {
+      label: 'bulma',
+      icon: 'https://jgthms.com/web-design-in-4-minutes/bulma.png'
+    },
+    {
+      label: 'fundamental-ui',
+      icon: 'http://sap.github.io/techne/images/home-designers-letters.png'
+    }
+  ];
+  activeUI = this.systems[0].label;
+  activePanel = 'alert';
 }
